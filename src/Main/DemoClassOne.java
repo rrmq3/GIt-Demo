54fbbdbbd2ba1f9522b37cf3b767cbef1ad61c6b
rrmq3 <rrmq3@mst.edu>
2018-02-18 20:52:31 -0600

    Update DemoClassOne.java
    Change made by other user

--- src/Main/DemoClassOne.java
+++ src/Main/DemoClassOne.java
@@ -3,6 +3,6 @@ package Main;
 public class DemoClassOne {
     public static void main(String[]args)
     {
-        System.out.println("Printing demo class one"); System.out.println("This statement is added by some other user");
+        System.out.println("Printing demo class one");
     }
 }
