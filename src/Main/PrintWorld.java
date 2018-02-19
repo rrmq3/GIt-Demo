c492f2d22003c8560cd8dc221212075a4a07c160
Ravindra <ravindrababu1220@gmail.com>
2018-02-18 20:11:59 -0600

    Initial commit

---
+++ src/Main/PrintWorld.java
@@ -0,0 +1,12 @@
+package Main;
+
+public class PrintWorld {
+
+    static String name = "Ravindra";
+
+
+    public static void main(String[]args)
+    {
+        System.out.println("My first print statement:  " + name);
+    }
+}
